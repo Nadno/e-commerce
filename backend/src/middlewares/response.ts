80c1d40f09a156d1d66df0b976ecee212a3dc811
Nadno <eunadno@gmail.com>
2021-03-16 19:08:54 -0300

    refactor: implement a Class syntax

--- backend/src/middlewares/response.ts
+++ backend/src/middlewares/response.ts
@@ -1,5 +1,4 @@
-import { NextFunction, Request, Response } from 'express';
-import { ResponseHandle } from '../response';
+import { NextFunction, Response } from 'express';
 
 const STATUS_CODE_OK = 200;
 const STATUS_CODE_BAD_REQUEST = 400;
@@ -7,49 +6,56 @@ const STATUS_CODE_UNAUTHORIZED = 401;
 const STATUS_CODE_NOT_FOUND = 404;
 const STATUS_CODE_SERVER_ERROR = 500;
 
-const jsonOk: ResponseHandle = function (this: Response, data = {}) {
-  const status = STATUS_CODE_OK;
-  this.status(status);
+class HandleResponse {
+  constructor() {
+    this.response = this.response.bind(this);
+  }
 
-  return this.json({ ...data, status });
-};
+  private jsonOk(this: Response, data = {}): Response  {
+    const status = STATUS_CODE_OK;
+    this.status(status);
 
-const jsonBadRequest: ResponseHandle = function (this: Response, data = {}) {
-  const status = STATUS_CODE_BAD_REQUEST;
-  this.status(status);
+    return this.json({ ...data, status });
+  };
 
-  return this.json({ ...data, status });
-};
+  private jsonBadRequest(this: Response, data = {}): Response  {
+    const status = STATUS_CODE_BAD_REQUEST;
+    this.status(status);
 
-const jsonUnauthorized: ResponseHandle = function (this: Response, data = {}) {
-  const status = STATUS_CODE_UNAUTHORIZED;
-  this.status(status);
+    return this.json({ ...data, status });
+  };
 
-  return this.json({ ...data, status });
-};
+  private jsonUnauthorized(this: Response, data = {}): Response  {
+    const status = STATUS_CODE_UNAUTHORIZED;
+    this.status(status);
 
-const jsonNotFound: ResponseHandle = function (this: Response, data = {}) {
-  const status = STATUS_CODE_NOT_FOUND;
-  this.status(status);
+    return this.json({ ...data, status });
+  };
 
-  return this.json({ message: 'request not found', ...data, status });
-};
+  private jsonNotFound(this: Response, data = {}): Response  {
+    const status = STATUS_CODE_NOT_FOUND;
+    this.status(status);
 
-const jsonServerError: ResponseHandle = function (this: Response, data = {}) {
-  const status = STATUS_CODE_SERVER_ERROR;
-  this.status(status);
+    return this.json({ message: 'request not found', ...data, status });
+  };
 
-  return this.json({ ...data, status });
-};
+  private jsonServerError(this: Response, data = {}): Response {
+    const status = STATUS_CODE_SERVER_ERROR;
+    this.status(status);
 
-const response = (req: Request, res: Response, next: NextFunction) => {
-  res.jsonOk = jsonOk;
-  res.jsonBadRequest = jsonBadRequest;
-  res.jsonUnauthorized = jsonUnauthorized;
-  res.jsonNotFound = jsonNotFound;
-  res.jsonServerError = jsonServerError;
+    return this.json({ ...data, status });
+  };
 
-  next();
-};
+  public response(_, res: Response, next: NextFunction): void {
+    res.jsonOk = this.jsonOk;
+    res.jsonBadRequest = this.jsonBadRequest;
+    res.jsonUnauthorized = this.jsonUnauthorized;
+    res.jsonNotFound = this.jsonNotFound;
+    res.jsonServerError = this.jsonServerError;
+  
+    next();
+  };
+}
 
-export default response;
+
+export default new HandleResponse().response;
