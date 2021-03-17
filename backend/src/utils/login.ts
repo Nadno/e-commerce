d8eefdcbd0b308a7468507803a01d52183eea2a8
Nadno <eunadno@gmail.com>
2021-03-17 08:42:35 -0300

    update checkLogin to return jwtVersion

--- backend/src/utils/login.ts
+++ backend/src/utils/login.ts
@@ -11,18 +11,18 @@ const checkLogin = async ({
   password,
 }: Login) => {
   const [account] = await db('users')
-    .select('name', 'avatar', 'id', 'password')
+    .select(['name', 'avatar', 'id', 'password', 'jwtVersion'])
     .where('users.email', '=', email);
 
   const ERROR_MESSAGE = 'Senha ou e-mail incorretos';
   if (!account) throw ERROR_MESSAGE;
   
-  const { password: dbPassword, ...rest } = account;
+  const { password: dbPassword, jwtVersion, ...rest } = account;
 
   const match = bcrypt.compareSync(password, dbPassword);
   if (!match) throw ERROR_MESSAGE;
   
-  return rest;
+  return { account: rest, jwtVersion };
 };
 
 export default checkLogin;
