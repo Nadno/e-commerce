fc8496c1fab22fd3b0a6a31e2de0cd629c2fa874
Nadno <eunadno@gmail.com>
2021-03-17 08:41:48 -0300

    add order_id and request body to response

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -3,12 +3,21 @@ import db from '../database/connection';
 
 class Order {
   public async create(req: Request, res: Response): Promise<Response> {
-	  const { id, products } = req.body;
-	  
-	  const [account] = await db('users').select('*').where('users.id', '=', id);
-	  if (!account) return res.jsonBadRequest({ message: 'Ocorreu um erro ao criar seu pedido, recarregue a página ou entre mais tarde.' });
-	  
-	  return res.jsonOk({ account, products });
+    const { id, products } = req.body;
+
+    const [account] = await db('users').select('*').where('users.id', '=', id);
+    if (!account)
+      return res.jsonBadRequest({
+        message:
+          'Ocorreu um erro ao criar seu pedido, recarregue a página ou entre mais tarde.',
+      });
+
+    const { password, jwtVersion, ...restAccount } = account;
+    return res.jsonOk({
+      ...req.body,
+      account: restAccount,
+      order_id: 'asd72dSA32sa4SA7aAS',
+    });
   }
 }
 
